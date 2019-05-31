9ea920314ca1e2fcb1b4d0949c04c2addce87b7b
FedotovaAnastasia <fines179@mail.ru>
2019-05-31 15:34:03 +0300

    task8.2

--- Task8/Task 8.2.js
+++ Task8/Task 8.2.js
@@ -1,34 +1,23 @@
-function abc() {
-let a = prompt("Введите первое число");
-return +a;
-}
-function cba() {
-	var a = function abc();
-	var b = function abc();
-	var c = function abc();
-	if (a>b && b>c) {
-		console.log(c);
-		console.log(b);
-		console.log(a);
-	} else if (c>b && b>a) {
-		console.log(a);
-		console.log(b);
-		console.log(c);
-	} else if (a>c && c>b) {
-		console.log(b);
-		console.log(c);
-		console.log(a);
-	} else if (c>a && a>b) {
-		console.log(b);
-		console.log(a);
-		console.log(c);
-	} else if (b>c && c>a) {
-		console.log(a);
-		console.log(c);
-		console.log(b);
-	} else if (b>a && a>c) {
-		console.log(c);
-		console.log(a);
-		console.log(b);
-	}
-}
+var carModel = prompt('enter car brand','');
+switch (carModel) {
+	case 'Ford':
+		console.log('Ford – страна происхождения США');
+		break;
+	case 'BMW':
+		console.log('BMW – страна происхождения Германия');
+		break;
+	case 'Peugeot':
+		console.log('Peugeot – страна происхождения Франция');
+		break;
+	case 'Lada':
+		console.log('Lada – страна происхождения Россия');
+		break;
+	case 'Mitsubishi':
+		console.log('Mitsubishi – страна происхождения Япония');
+		break;
+	case 'Porsche':
+		console.log('Porsche – страна происхождения Германия');
+		break;
+	default:
+		console.log(carModel + ' -  страна происхождения неизвестна');
+} 
