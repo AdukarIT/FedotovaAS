9e1db0b163fea71a2c52d72f40d51178bf9afb14
FedotovaAnastasia <fines179@mail.ru>
2019-06-03 13:23:43 +0300

    changed js

--- Task8/Task 8.6.js
+++ Task8/Task 8.6.js
@@ -1,3 +1,10 @@
-for (i = 0; i <= 15; i++) {
-    
+for (i = 1; i <= 8; i++) {
+	a = " " + "#";
+	b = "#" + " ";
+	c = "\n";
+	if (i % 2 === 0) {
+		console.log(a + a + a + a + c);
+	} else { 
+		console.log(b + b + b + b + c);
+	}
 }
