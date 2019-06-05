260d39bb12477dfb790d80564acf78990bae1d13
FedotovaAnastasia <fines179@mail.ru>
2019-06-05 10:27:40 +0300

    js

--- Task8/Task 8.3.js
+++ Task8/Task 8.3.js
@@ -1,9 +1,11 @@
 var year;
 do {
 	year = prompt('Enter year','');
-} while (isNaN(year) || year <= 0);
-if (year % 4 === 0) {
+} while(isNaN(year) || year <= 0);
+if(year % 4 === 0 &&  year % 100 != 0) {
 	alert("a leap year");
+} else if (year % 400 === 0) {
+	alert("a leap year");	
 } else {
 	alert("not a leap year");
 }
