9edd5105d776bf2bcd02933d4a0d28541e88d015
FedotovaAnastasia <fines179@mail.ru>
2019-06-05 09:31:00 +0300

    js

--- Task8/Task 8.5.js
+++ Task8/Task 8.5.js
@@ -1,9 +1,9 @@
 var presentValue = 0, result;
-for (i = 50; i >0; i--) {
+for (i = 50; i > 0; i--) {
     if (i % 2 === 0) {
         continue;
     } else {
         result = presentValue + i; 
-        presentValue = sum;
+        presentValue = result;
     }
 }
