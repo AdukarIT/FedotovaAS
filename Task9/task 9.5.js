6db8e1feadf70497c8f200529a465a974c8e1d76
AnastasiaFedotova <fines179@mail.ru>
2019-06-07 00:07:12 +0300

    added task 9.5

---
+++ Task9/task 9.5.js
@@ -0,0 +1,18 @@
+function getRoot(a, b, c) {
+    var Diskr, x1, x2;
+    if (a === 0) {
+       return false;
+    } else {
+        Diskr = b ** 2 - 4 * a * c;
+        if (Diskr > 0) {
+            x1 = ((0 - b) + Math.sqrt(Diskr)) / (2 * a);
+            x2 = ((0 - b) - Math.sqrt(Diskr)) / (2 * a)
+            return x1 + " " + x2;
+        } else if (Diskr == 0) {
+            x1 = (0 - b) / (2 * a)
+            return x1;
+        } else {
+            return "нет корней"
+        }
+    }
+}
