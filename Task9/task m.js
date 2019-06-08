d34c63a4d5eebe4604711dcd42ebbdca7b6eca3c
AnastasiaFedotova <fines179@mail.ru>
2019-06-08 11:38:12 +0300

    changed

--- Task9/task m.js
+++ Task9/task m.js
@@ -3,8 +3,8 @@ function reverseNum(num) {
      if(num >= 10) {
          remainder = num % 10;
          num = (num - remainder) / 10;
-         var revers = reverseNum(num);
-         return String(remainder) + revers;
+         var a = reverseNum(num);
+         return String(remainder) + a;
      } else if (num < 10) {
          return num ;
      }
