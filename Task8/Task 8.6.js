0aa5190e2945e43b5ff889653e65c396e1824250
FedotovaAnastasia <fines179@mail.ru>
2019-06-13 14:25:57 +0300

    changed

--- Task8/Task 8.6.js
+++ Task8/Task 8.6.js
@@ -1,3 +1,4 @@
+//Используя циклы, выведите в консоль первые 15 чисел Фибоначчи.
 var presentValue1 = 0, presentValue2 = 1, previousValue, result; 
 for (i = 0; i < 15; i++) { 
 	previousValue = presentValue1 + presentValue2; 
