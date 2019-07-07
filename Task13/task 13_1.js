e26f6028b4ef870fbf90aa1930903dfc5896f7c5
Anastasia <Anastasia@MacBook-Air-Ila.local>
2019-07-08 00:25:30 +0300

    added task 13

---
+++ Task13/task 13_1.js
@@ -0,0 +1,6 @@
+/*Напишите функцию, которая принимает в качестве параметра номер символа в таблице Unicode. 
+Подсчитайте количество таких символов во всех элементах массива, как в задаче 3 из первой части практики. */
+function getSymbolInUnicode(numUnic) {
+    let symbol = String.fromCharCode(numUnic);
+    return symbol;
+}
