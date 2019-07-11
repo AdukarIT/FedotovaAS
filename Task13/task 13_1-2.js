1730fac986337967612cdf2ec2ba42ddc4a97fde
FedotovaAnastasia <fines179@mail.ru>
2019-07-11 16:52:04 +0300

    changed task13

--- Task13/task 13_1-2.js
+++ Task13/task 13_1-2.js
@@ -1,13 +1,13 @@
-/*Напишите функцию, которая принимает в качестве параметра номер символа в таблице Unicode. 
+/*Напишитео/*Напишите функцию, которая принимает в качестве параметра номер символа в таблице Unicode. 
 Подсчитайте количество таких символов во всех элементах массива, как в задаче 3 из первой части практики. */
-function getSymbolInUnicode(numUnic, arr) {
+function countСharacters(numUnic, arr) {
 	let str = arr.join('');
-	let num = String.fromCharCode(numUnic);
-	let numberOfCharacters = 0;
+	let symbol = String.fromCharCode(numUnic);
+	let count = 0;
 	for(i = 0; i < str.length; i++) {
-		if(str[i] == num) numberOfCharacters++;
+		if(str[i] == symbol) count++;
     	}
-	return numberOfCharacters;
+	return count;
 }
 let arrSymbol = ['рапира', 'арбат' , 'привет' , 'ра', 'дом'];
 getSymbolInUnicode(1088, arrSymbol); // 1088 - 'р'
@@ -15,17 +15,17 @@ getSymbolInUnicode(1088, arrSymbol); // 1088 - 'р'
  фиксируя искомый символ. Например:
  let countOfD = letterCounter(100);	// 100 = 'd' в Unicode
 console.log(countOfD(["dad", "rod", "doctor"])); // 4*/
-function getSymbolInUnicode2(numUnic) {    
-	let num = String.fromCharCode(numUnic);
-	let numberOfCharacters = 0;
+function countСharacters2(numUnic) {    
+	let symbol = String.fromCharCode(numUnic);
+	let count = 0;
 	function colculate(arr){
 		let str = arr.join('');
 		for(i = 0; i < str.length; i++) {
-			if(str[i] == num) numberOfCharacters++;
+			if(str[i] == symbol) count++;
 		}
-		return numberOfCharacters;
+		return count;
 	}
 	return colculate;
 }
-let count = getSymbolInUnicode2(1088);
-count(['ра', 'а' , 'п' , 'р', 'д']);
+let countSymbol = countСharacters2(1088);
+countSymbol(['ра', 'а' , 'п' , 'р', 'д']);
