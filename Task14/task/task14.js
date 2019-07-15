029edf6b6de06a5fab23297419b4e8b67d7fbebe
Anastasia <Anastasia@MacBook-Air-Ila.local>
2019-07-15 22:44:40 +0300

    changed

--- Task14/task/task14.js
+++ Task14/task/task14.js
@@ -5,25 +5,43 @@ if(elem == 'www') document.getElementById('elem').classList.remove('www');
 //3 Напишите функцию поиска recursiveSearchTags, которая принимает на вход document и имя тега, 
 //а возвращает массив из всех элементов соответствующих этому тегу. Поиск идет в теге body
 function recursiveSearchTags(document, tag) {
-  return document.body.querySelectorAll()
+	let tags = document.body.getElementsByTagName(tag);
+	let arr = [];
+	for(i = 0; i < tags.length; i++) {
+		arr.push(tags[i]);
+	}
+	return arr;
+
 }
+recursiveSearchTags(document, 'div');
 //4 Дан ul. Дан массив. Вставьте элементы этого массива в конец ul так, чтобы каждый элемент стоял в своем li. 
 //Сделайте так, чтобы четные позиции имели красный фон.
 let ul = document.getElementsByTagName('ul')[0];
-let arr = ['1','2','3','4','5'];
+let arr = ['Пришел слон', 'Пришла слониха', 'Пришел', 'маленький', 'слоненок'];
+
 for(i = 0; i < arr.length; i++) {
-	ul.appendChild(document.createElement('li'));
+	if(i % 2 != 0) ul.appendChild(document.createElement('li'));
+	else {
+		ul.appendChild(document.createElement('li'));
+		ul.children[i].style.backgroundColor = 'red'
+	}
 }
+
 let li = document.getElementsByTagName('li');
+
 for(i = 0; i < arr.length; i++){
-	if(i % 2 != 0)li[i].innerHTML = arr[i];
-	else 
+	li[i].innerHTML = arr[i];
 }
 //5 Дан элемент #elem. Найдите его соседа сверху и добавьте ему в конец текст '!'
 let textNeigbor = document.getElementById('elem').previousElementSibling.appendChild(document.createTextNode('!'));
 //6Реализуйте функцию wrapInParagraph, которая находит текст (дочерние текстовые ноды) 
 //внутри элемента div и оборачивает текст в параграф
-
+function wrapInParagp() {
+	let divs = document.getElementsByTagName('div');
+	for(i = 0; i < divs.length; i++) {
+		childNodes;
+	}
+}
 //7 Реализуйте функцию normalizeClassNames, которая нормализует имена классов для всех элементов на странице. 
 //В данном случае это означает что происходит преобразование всех классов написанных используя kebab нотацию 
 //в camelCase нотацию: text-center => textCenter
