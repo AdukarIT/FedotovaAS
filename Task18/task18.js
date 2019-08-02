9b360208991b445f68aaa73613bfe994f9740db9
FedotovaAnastasia <fines179@mail.ru>
2019-08-02 17:00:15 +0300

    changed

--- Task18/task18.js
+++ Task18/task18.js
@@ -19,10 +19,21 @@ let objInObj = {
 }
 
 let objInObjJson = JSON.stringify(objInObj);
-JSON.parse(objInObjJson)ж
+JSON.parse(objInObjJson);
 
 /*Используя ссылку 
 “https://api.instagram.com/v1/users/2093101329/media/recent/?access_token=2093101329.0e4abd3.d017a21b3e6e45408126e42cf0940d79” получите данные, найдите изображения и вставьте их 
 в документ, также оберните их в ссылки, используя свойства link.*/
 
+let xhr1 = new XMLHttpRequest;
+xhr1.open('GET', 'https://api.instagram.com/v1/users/2093101329/media/recent/?access_token=2093101329.0e4abd3.d017a21b3e6e45408126e42cf0940d79', false)
+xhr1.responseType = 'json';
+xhr.send();
 
+let response = JSON.parse(xhr1.response);
+
+function getIMG(arr) {
+	
+}
+
+let arrImg = getIMG(response);
