cae5d113cdfdc2c1a57df7b1014f064486fc198a
Anastasia <Anastasia@MacBook-Air-Ila.local>
2019-09-01 19:21:32 +0300

    changed 22

--- Task22/calendarScript.js
+++ Task22/calendarScript.js
@@ -1,34 +1,85 @@
-var options = {
-  month: 'long',
-};
+$(function() {
+	
+	var options = {
+	month: 'long',
+	year: 'numeric'
+	};
 
-let clicked = clickGetMonth();
+	let currentDate = new Date();
+	let year = currentDate.getFullYear();
+	let month = currentDate.getMonth();
 
-$('#date_picker').click(function() {
-	clicked();
-});
+	let clicked = clickGetMonth();
 
-function clickGetMonth() {
-	let open = false;
+	$('#date_picker').click(function() {
+		clicked();
+	});
 
-	return function() {
-		if(open == false) {
-			open = true;
+	function clickGetMonth() {
+		let open = false;
 
-			$('#calendar').css('display', 'grid');
-			$('#date_picker').removeClass().addClass('date_picker_checked');
-			$('#calendar_month_year').text(`${new Date().toLocaleString("ru", options)} ${new Date().getFullYear()}`);
-		} else if(open == true) {
-			open = false;
-			
-			$('#calendar').css('display', 'none');
-			$('#date_picker').removeClass().addClass('date_picker_unchecked');
+		return function() {
+			if(open == false) {
+				open = true;
+
+				$('#calendar').css('display', 'grid');
+				$('#date_picker').removeClass().addClass('date_picker_checked');
+				$('#calendar_month_year').text(`${currentDate.toLocaleString("ru", options)}`);
+
+				month = currentDate.getMonth();
+
+				let date = new Date(year, month, 1);
+				let firstDay = date.getDate();
+				let lastDay = new Date(year, month + 1, 0).getDate();
+				let table = new tableDate(date, firstDay, lastDay);
+				table.getTable();
+			} else if(open == true) {
+				open = false;
+				
+				$('#calendar').css('display', 'none');
+				$('#date_picker').removeClass().addClass('date_picker_unchecked');
+			}
 		}
 	}
-}
 
-class Data {
+	class tableDate {
+		constructor(date, firstDay, lastDay) {
+			this.firstDay = firstDay;
+			this.lastDay = lastDay;
+			this.date = date;
+		}
+		getTable() {
+			
+			let dayWeek = this.date.getDay();
+
+			$('#calendar_table').empty();
+
+			for(let i = 1; i <= this.lastDay; i ++) {
+				$('#calendar_table').append(`<td>${i}</td>`);
+			}
+
+		}
+	}
+
+	$('#calendar_forward').click(function() {
+		$('#calendar_month_year').text(`${new Date(year, month += 1, 1).toLocaleString("ru", options)}`);
+		
+		let date = new Date(year, month, 1);
+		let firstDay = date.getDate();
+		let lastDay = new Date(year, month + 1, 0).getDate();
+		let table = new tableDate(date, firstDay, lastDay);
+		table.getTable();
+	});
+
+	$('#calendar_back').click(function() {
+		$('#calendar_month_year').text(`${new Date(year, month -= 1, 1).toLocaleString("ru", options)}`);
+		
+		let date = new Date(year, month, 1);
+		let firstDay = date.getDate();
+		let lastDay = new Date(year, month + 1, 0).getDate();
+		let table = new tableDate(date, firstDay, lastDay);
+		table.getTable();
+	});
+});
+		
 	
-}
-
-//
