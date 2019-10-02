9b527158a33bf0202b3e8c2196f9944289e6491e
Anastasia <Anastasia@MacBook-Air-Ila.local>
2019-10-02 23:17:16 +0300

    changed

--- Task27/src/js/Gallery.js
+++ Task27/src/js/Gallery.js
@@ -13,37 +13,41 @@ class Gallery {
         this.basket = basket; 
         this.basketBooks = []; //массив книг для корзины
 
-        this.loadRedrow();
+        this.load();
         this.$buttonBasket.click(() => this.openBasket());
     }
     //загружаем 
     //прорисовываем
-    loadRedrow() {
+    load() {
         this.books = JSON.parse(window.localStorage.getItem('books')) ? JSON.parse(window.localStorage.getItem('books')) : [];
-        this.books.forEach(book => {
-            this.$ul.append(`<li data-id="${book.id} class="card">
-                    <img src="https://i.pinimg.com/736x/e1/b8/c3/e1b8c3cc853fe4e5c99ae93e0c8977b6.jpg" class="card-img-top" alt="...">
-                    <div class="card-body" data-id="${book.id}">
-                        <h5 class="product__list__title card-title">
-                            ${book.title}
-                        </h5>
-                        <p class=".product__list__author">
-                            ${book.author}
-                        </p>
-                        <p class="product__list__item">
-                            ${book.prise}
-                        </p>
-                        <p class="product__list__item card-text">
-                            ${book.description}
-                        </p>
-                        <button class="btn btn-info ${this.gallery_button}" type="button">
-                            В корзину
-                        </button>
-                    </div> 
-                </li>`);
-              });     
+        this.redrow();
         this.$button = this.$gallery.find(`.${this.gallery_button}`);
         this.event();
+        
+    }
+    redrow() {
+        this.books.forEach(book => {
+            this.$ul.append(`<li data-id="${book.id} class="card">
+                <img src="https://i.pinimg.com/736x/e1/b8/c3/e1b8c3cc853fe4e5c99ae93e0c8977b6.jpg" class="card-img-top" alt="...">
+                <div class="card-body" data-id="${book.id}">
+                    <h5 class="product__list__title card-title">
+                        ${book.title}
+                    </h5>
+                    <p class=".product__list__author">
+                        ${book.author}
+                    </p>
+                    <p class="product__list__item">
+                        ${book.prise}
+                    </p>
+                    <p class="product__list__item card-text">
+                        ${book.description}
+                    </p>
+                    <button class="btn btn-info ${this.gallery_button}" type="button">
+                        В корзину
+                    </button>
+                </div> 
+            </li>`);
+        });                 
     }
 
     event() {
@@ -52,14 +56,16 @@ class Gallery {
             debugger;
             let parentID = event.target.parentElement.dataset.id;
             event.target.innerHTML = 'уже добавлено';
-            event.target.setAttribute('disabled', 'disabled')
-            this.basketBooks.push({
-                id: parentID,
-                count: 1,
-            })
-            this.basket.redrow(this.basketBooks);
-        });
-              
+            event.target.setAttribute('disabled', 'disabled');
+            let repeatСheck = this.basketBooks.find((book) => book.id == parentId)
+            if(this.basketBooks.length == 1 || repeatСheck == undefined) {
+                this.basketBooks.push({
+                    id: parentID,
+                    count: 1,
+                })
+            }
+            window.localStorage.setItem('booksId', JSON.stringify(this.basketBooks));        
+        });      
     }
 
     openBasket() {
